8b2432a54c911d67225f6fb936b699dcf5406614
JoseLuisPucChan <josechan211@gmail.com>
2016-09-20 01:12:50 -0500

    Se agrego en formulario Bibliteca

---
+++ ProyectoUTM/UTM.XML/GUI/frmGestionBliblioteca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTM.XML.GUI
+{
+    public partial class frmGestionBliblioteca : Form
+    {
+        public frmGestionBliblioteca()
+        {
+            InitializeComponent();
+        }
+    }
+}
